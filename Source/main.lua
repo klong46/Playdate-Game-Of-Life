0633580291dc202ad52267088394ab6f37db5662
klong46 <kyledlong1@gmail.com>
2023-09-03 23:06:13 -0400

    add cursor move and select functionality

--- Source/main.lua
+++ Source/main.lua
@@ -4,25 +4,24 @@ local gfx = playdate.graphics
 
 CRANK_SPEED = 6;
 
-SquaresPerRow = 40;
-RectWidth = 400/SquaresPerRow
-RectHeight = 240/SquaresPerRow
-PickerPos = {x = 15, y = 15}
+SQUARES_PER_SIDE = 20;
+RectWidth = 400/SQUARES_PER_SIDE
+RectHeight = 240/SQUARES_PER_SIDE
+Cursor = {x = 12, y = 12}
 Frames = 1
-Done = false
 
 CellMat = {}
-for i=1,SquaresPerRow do
+for i=1,SQUARES_PER_SIDE do
     CellMat[i] = {}
-  for j=1,SquaresPerRow do
+  for j=1,SQUARES_PER_SIDE do
     CellMat[i][j] = 0
   end
 end
 
 NextFrame = {}
-for i=1,SquaresPerRow do
+for i=1,SQUARES_PER_SIDE do
     NextFrame[i] = {}
-  for j=1,SquaresPerRow do
+  for j=1,SQUARES_PER_SIDE do
     NextFrame[i][j] = 0
   end
 end
@@ -48,8 +47,8 @@ local function getLiveNeighbors(x, y)
     local liveNeighbors = 0
     for i = -1, 1, 1 do
         for j = -1, 1, 1 do
-            local validX = not (x + i == 0 or x + i > SquaresPerRow)
-            local validY = not (y + j == 0 or y + j > SquaresPerRow)
+            local validX = not (x + i == 0 or x + i > SQUARES_PER_SIDE)
+            local validY = not (y + j == 0 or y + j > SQUARES_PER_SIDE)
             local notSame = not (i == 0 and j == 0)
             if validX and validY and notSame then
                 if CellMat[x+i][y+j] == 1 then
@@ -66,14 +65,14 @@ end
 local function iterateMatrix(case)
     local xPos = 0
     local yPos = 0
-    for i = 1, SquaresPerRow, 1 do
-        for j = 1, SquaresPerRow, 1 do
+    for i = 1, SQUARES_PER_SIDE, 1 do
+        for j = 1, SQUARES_PER_SIDE, 1 do
             if case == 'update' then
-                local notPickerPos = not (i == PickerPos.x and j == PickerPos.y)
-                xPos = (j-1) * RectWidth
-                yPos = (i-1) * RectHeight
+                local notCursor = not (i == Cursor.x and j == Cursor.y)
+                xPos = (i-1) * RectWidth
+                yPos = (j-1) * RectHeight
                 setCellColor(i,j)
-                if notPickerPos then
+                if notCursor then
                     -- print('i: ' .. i .. '  j: ' .. j)
                     gfx.fillRect(xPos, yPos, RectWidth, RectHeight)
                 end
@@ -95,23 +94,66 @@ local function iterateMatrix(case)
     end
 end
 
-local function createCursor()
-    gfx.setColor(gfx.kColorBlack)
-    local checkerboard = { 0xaa, 0x55, 0xaa, 0x55, 0xaa, 0x55, 0xaa, 0x55 }
-    gfx.setPattern(checkerboard)
-    gfx.setLineWidth(1)
-    gfx.fillRect((PickerPos.x-1) * RectWidth, (PickerPos.y-1) * RectHeight, RectWidth, RectHeight)
+local function drawCursor()
+    gfx.fillRect((Cursor.x-1) * RectWidth, (Cursor.y-1) * RectHeight, RectWidth, RectHeight)
 end
 
+local function getCheckerboard()
+    local checkerboard = { 0xaa, 0x55, 0xaa, 0x55, 0xaa, 0x55, 0xaa, 0x55 }
+    gfx.setPattern(checkerboard)
+end
+
+local function setCursor()
+    getCheckerboard()
+    drawCursor()
+end
+
+local function deletePreviousCursor()
+    setCellColor(Cursor.x, Cursor.y)
+    drawCursor()
+end
+
+local function moveCursor(x, y)
+    deletePreviousCursor()
+    Cursor.x += x
+    Cursor.y += y
+    setCursor()
+end
+
+function playdate.leftButtonDown()
+    if Cursor.x > 1 then
+        moveCursor(-1,0)
+    end
+end
+
+function playdate.rightButtonDown()
+    if Cursor.x < SQUARES_PER_SIDE then
+        moveCursor(1,0)
+    end
+end
+
+function playdate.upButtonDown()
+    if Cursor.y > 1 then
+        moveCursor(0,-1)
+    end
+end
+
+function playdate.downButtonDown()
+    if Cursor.y < SQUARES_PER_SIDE then
+        moveCursor(0,1)
+    end
+end
+
+function playdate.AButtonDown()
+    CellMat[Cursor.x][Cursor.y] = 1
+end
 
 iterateMatrix('update')
-createCursor()
+setCursor()
 function playdate.update()
     local ticks = playdate.getCrankTicks(CRANK_SPEED)
     Frames += ticks
     if ticks ~= 0 then
-        -- gfx.clear()
-        -- createPicker()
         iterateMatrix('next')
         iterateMatrix('set')
         iterateMatrix('update')
